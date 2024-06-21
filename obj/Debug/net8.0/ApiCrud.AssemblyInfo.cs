ab2d29cb070d66406a3f2084631cb9ec11cb5e70
alissonthx <alissonthx@github.com>
2024-06-21 15:00:47 -0300

    add: minor changes at launch settings

--- obj/Debug/net8.0/ApiCrud.AssemblyInfo.cs
+++ obj/Debug/net8.0/ApiCrud.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ApiCrud")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2af77376e10db3b5e289d38fa3004c23e6d0a32f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3fea238b2091ecd8fdec5c77323a1bcf89aa7149")]
 [assembly: System.Reflection.AssemblyProductAttribute("ApiCrud")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ApiCrud")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
